6ff1bfe8ff3399a441da4c93cd9320f0c2e1be73
Mochammad Rizky Diprasetya <software@rizkydiprasetya.com>
2024-12-31 08:35:56 +0100

    change metadata info

--- src/setup.py
+++ src/setup.py
@@ -1,23 +1,17 @@
 from setuptools import setup
 
 
-setup(name='mypackage',
-version='1.0.0',
-description='MLPro: Integration River',
-author='MLPro Team',
-author_mail='mlpro@listen.fh-swf.de',
-license='Apache Software License (http://www.apache.org/licenses/LICENSE-2.0)',
-packages=['mypackage'],
-
-# Package dependencies for full installation
-extras_require={
-    "full": [
-        "dill",
-        "numpy",
-        "matplotlib",
-        "multiprocess",
-        "mlpro"
-    ],
-},
-
-zip_safe=False)
+setup(
+    name="mlpro-int-tensorboard",
+    version="0.0.1",
+    description="MLPro: Integration Tensorboard",
+    author="Mochammad Rizky Diprasetya",
+    author_mail="software@rizkydiprasetya.com",
+    license="Apache Software License (http://www.apache.org/licenses/LICENSE-2.0)",
+    packages=["mlpro_int_tensorboard"],
+    # Package dependencies for full installation
+    extras_require={
+        "full": ["dill", "numpy", "matplotlib", "multiprocess", "mlpro"],
+    },
+    zip_safe=False,
+)
